541583aa4f7f764efdae146280d32d282fd57fc8
Saima Usman <saimausman.dxb@gmail.com>
2025-11-05 20:46:17 +0400

    Task#8

---
+++ common_char.py
@@ -0,0 +1,15 @@
+# This program counts & prints the number of common characters in two given strings.
+
+str1 = input("Enter first string: ")
+str2 = input("Enter second string: ")
+
+common = ""
+
+for char in str1:
+    if char in str2 and char not in common:
+        common += char
+
+if common:
+    print("Common characters:", common)
+else:
+    print("No common characters found.")
